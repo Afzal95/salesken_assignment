9d6583581233d0b11f98e27d5397da41546a49ac
Afzal95 <afzalpolite@gmail.com>
2021-09-06 04:18:56 +0530

    comments added on html and javascript files,code refactored

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 
+//  declaring interval variable and initialising time value
+
+var interval;
 var time =1;
-var playFlag = false;
+
+// this function runs as soon as page loads, and this runs func can that makes audio bars
+function bars(col){
+    for (let i = 1; i <=100; i++) {
+        let ran1 = Math.floor(Math.random()*100+90);
+        let ran2 = Math.floor(Math.random()*100+110);
+        let ran = Math.max(ran1,ran2);
+        if(i%5==0 || i%19==0){
+            can(ran,i,col);
+        }else{
+            can(ran,i,col)
+        }
+    }
+ }
+bars(); 
+
+//  This function runs and creates audio bars
 
 function can(m,id,color="grey"){
 
@@ -17,44 +36,20 @@ function can(m,id,color="grey"){
     context.fillRect(0,0,80,150);
     div.appendChild(canvas);
 }
-
- function bars(col){
-    for (let i = 1; i <=100; i++) {
-        let ran1 = Math.floor(Math.random()*100+90);
-        let ran2 = Math.floor(Math.random()*100+110);
-        let ran = Math.max(ran1,ran2);
-        random.push(ran);
-        if(i%5==0 || i%19==0){
-            can(ran,i,col);
-        }else{
-            can(ran,i,col)
-        }
-    }
- }
-bars();
-
-function update(m,id,color="grey"){
-
-    canvas.setAttribute("id",`id${id}`);
-    canvas.style.width="10px";
-    canvas.style.height="150px";
-    context.fillStyle=`${color}`;
-    context.fillRect(0,0,80,150);
-    div.appendChild(canvas);
-}
-
-var interval;
+// function for playing and pausing audio player
 function play(){
     let aud = document.getElementById("audio");
-//    aud.currentTime=20;
-    console.log(aud.currentTime);
-    console.log(aud.duration);
 
     if(interval==undefined){
         aud.play();
         document.getElementById("play").innerHTML=`<i class="fas fa-pause"></i>`
         interval=setInterval(()=>{
             time++;
+            if(time>100){
+                clearInterval(interval);
+                aud.pause();
+                document.getElementById("play").innerHTML=`<i class="fas fa-play"></i>`        
+            }
             colorUpdate();
         },1000)
     }else{
@@ -66,13 +61,14 @@ function play(){
 
 }
 
+// function for changing colors of audio bars
 function colorUpdate(){
     var canvases = document.querySelectorAll("canvas");
     for (let i = 0; i < canvases.length; i++) {
         var ctx = canvases[i].getContext("2d");
         ctx.clearRect(0, 0, canvases[i].width, canvases[i].height);
         if(canvases[i].id<time){
-            ctx.fillStyle="red";
+            ctx.fillStyle="magenta";
             ctx.fillRect(0,0,80,150);
         }else{
             ctx.fillStyle="grey";
@@ -81,13 +77,20 @@ function colorUpdate(){
     }
 }
 
+// adding event listener "click" to every bar ie- every canvas
 document.querySelectorAll("canvas").forEach(el=>{
     el.addEventListener("click",updateBar);
 });
 
+// this function triggers when any bar is clicked 
 function updateBar(e){
     var tgt = e.target.id;
     let aud = document.getElementById("audio");
     aud.currentTime=tgt;
     time=tgt;
+    if(aud.currentTime>tgt){
+        aud.play();
+    }
 }
+
+// creating tags 

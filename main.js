4ebe0d0932a618f72e19ce8e530602fe62b8ae25
Afzal95 <afzalpolite@gmail.com>
2021-09-06 01:38:46 +0530

    audio added, audio wave bar added and matched with audio,play pause button functionaning

--- main.js
+++ main.js
@@ -1,56 +1,93 @@
-function can(m,id){
+
+var time =1;
+var playFlag = false;
+
+function can(m,id,color="grey"){
+
     var div = document.getElementById("main_div");
     var canvas = document.createElement("canvas");
     if(m>0){
         canvas.style.marginTop = `${m}px`;
     }
     var context = canvas.getContext("2d");
-    // canvas.style.padding="10px";
-    canvas.setAttribute("id",`id${id}`)
+    canvas.setAttribute("id",`${id}`);
     canvas.style.width="10px";
     canvas.style.height="150px";
-    // canvas.width=3;
-    // canvas.height=200;
-    context.fillStyle="grey";
-    context.fillRect(0,0,100,150);
+    context.fillStyle=`${color}`;
+    context.fillRect(0,0,80,150);
     div.appendChild(canvas);
 }
+
  function bars(col){
     for (let i = 1; i <=100; i++) {
         let ran1 = Math.floor(Math.random()*100+90);
         let ran2 = Math.floor(Math.random()*100+110);
         let ran = Math.max(ran1,ran2);
+        random.push(ran);
         if(i%5==0 || i%19==0){
-            can(ran,i);
+            can(ran,i,col);
         }else{
-            can(ran,i)
+            can(ran,i,col)
         }
     }
  }
 bars();
 
-function canNew(m,id){
-    const canvas = document.getElementById(`id${id}`);
-    // var canvas = document.createElement("canvas");
-    // if(m>0){
-    //     canvas.style.marginTop = `${m}px`;
-    // }
-    let context = canvas.getContext("2d");
-    // canvas.style.padding="10px";
-    // canvas.setAttribute("id",`id${id}`);
-    // canvas.style.width="10px";
-    // canvas.style.height="150px";
-    // canvas.width=3;
-    // canvas.height=200;
-    context.fillStyle="red";
-    context.fill();
-    // context.fillRect(0,0,100,150);
-//    div.appendChild(canvas);
+function update(m,id,color="grey"){
+
+    canvas.setAttribute("id",`id${id}`);
+    canvas.style.width="10px";
+    canvas.style.height="150px";
+    context.fillStyle=`${color}`;
+    context.fillRect(0,0,80,150);
+    div.appendChild(canvas);
 }
 
+var interval;
 function play(){
-    // for (let i = 1; i <= 100; i++) {
-    //     document.getElementById(`id${i}`).style.color="red";
-    //     console.log(document.getElementById(`id${i}`));
-    // }
+    let aud = document.getElementById("audio");
+//    aud.currentTime=20;
+    console.log(aud.currentTime);
+    console.log(aud.duration);
+
+    if(interval==undefined){
+        aud.play();
+        document.getElementById("play").innerHTML=`<i class="fas fa-pause"></i>`
+        interval=setInterval(()=>{
+            time++;
+            colorUpdate();
+        },1000)
+    }else{
+        aud.pause();
+        document.getElementById("play").innerHTML=`<i class="fas fa-play"></i>`        
+        clearInterval(interval);
+        interval=undefined;
+    }
+
+}
+
+function colorUpdate(){
+    var canvases = document.querySelectorAll("canvas");
+    for (let i = 0; i < canvases.length; i++) {
+        var ctx = canvases[i].getContext("2d");
+        ctx.clearRect(0, 0, canvases[i].width, canvases[i].height);
+        if(canvases[i].id<time){
+            ctx.fillStyle="red";
+            ctx.fillRect(0,0,80,150);
+        }else{
+            ctx.fillStyle="grey";
+            ctx.fillRect(0,0,80,150);
+        }
+    }
+}
+
+document.querySelectorAll("canvas").forEach(el=>{
+    el.addEventListener("click",updateBar);
+});
+
+function updateBar(e){
+    var tgt = e.target.id;
+    let aud = document.getElementById("audio");
+    aud.currentTime=tgt;
+    time=tgt;
 }
